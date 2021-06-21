e9917db97e9ab4490952022cb08d2d2f762adac2
EAAR-300519 <aspire5@outlook.es>
2021-06-21 01:25:42 -0500

    [Update] cotizador y email

---
+++ enviar.php
@@ -0,0 +1,30 @@
+<?php
+$nombre = $_POST['nombre'];
+$puesto = $_POST['puesto'];
+$empresa = $_POST['empresa'];
+$area = $_POST['area'];
+$etiquetas = $_POST['etiquetas'];
+$correo = $_POST['correo'];
+$numero = $_POST['numero'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' . $correo . " \r\n";
+/* $header .= "X-Mailer: PHP/" . phpversion() . " \r\n"; */
+/* $header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+ */
+$message = "Este mensaje fue enviado por: " . $nombre . " \r\n";
+$message = "Empresa: " . $empresa . " \r\n";
+$message .= "Su e-mail es: " . $correo . " \r\n";
+$message .= "Teléfono de contacto: " . $numero . " \r\n";
+$message .= "Tipo de etiqueta a cotizar: " . $etiquetas . " \r\n";
+$message .= "Comentarios sobre cotización: " . $_POST[$mensaje] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+
+$para = 'aspire5@outlook.es';
+$asunto = 'Mensaje de Prueba';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
