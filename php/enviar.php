54f7f06ea6431372175b91af1774e3badcb597c3
EAAR-300519 <aspire5@outlook.es>
2021-06-21 11:41:39 -0500

    [Update] email enviado

--- php/enviar.php
+++ php/enviar.php
@@ -2,14 +2,15 @@
 $nombre = $_POST['nombre'];
 $puesto = $_POST['puesto'];
 $empresa = $_POST['empresa'];
-$area = $_POST['area'];
+/* $area = $_POST['area']; */
+$area = $_POST['_combo'];
 $etiquetas = $_POST['etiquetas'];
 $correo = $_POST['correo'];
 $numero = $_POST['numero'];
 $mensaje = $_POST['mensaje'];
 
-echo "Hola $nombre tu puesto es: $puesto tu empresa es $empresa las etiquetas que deseas son $area 
-la cantidad son $etiquetas su correo es $correo y su número es $numero";
+/* echo "Hola $nombre tu puesto es: $puesto tu empresa es $empresa las etiquetas que deseas son $area 
+la cantidad son $etiquetas su correo es $correo y su número es $numero"; */
 
 $header = 'From: ' . $correo . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -20,14 +21,15 @@ $message = "Este mensaje fue enviado por: " . $nombre . " \r\n";
 $message = "Empresa: " . $empresa . " \r\n";
 $message .= "Su e-mail es: " . $correo . " \r\n";
 $message .= "Teléfono de contacto: " . $numero . " \r\n";
-$message .= "Tipo de etiqueta a cotizar: " . $etiquetas . " \r\n";
-$message .= "Comentarios sobre cotización: " . $_POST[$mensaje] . " \r\n";
+$message .= "Tipo de etiqueta a cotizar: " . $area . " \r\n";
+$message .= "Cantidad de etiquetas a cotizar: " . $etiquetas . " \r\n";
+$message .= "Comentarios sobre cotización: " . $mensaje . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
 
-$para = 'aspire5@outlook.es';
+$para = 'recklessmoney3@gmail.com';
 $asunto = 'Mensaje de Prueba';
 
-mail($para, $asunto, utf8_decode($message), $header);
+@mail($para, $asunto, utf8_decode($message), $header);
 
 header("Location:index.html");
