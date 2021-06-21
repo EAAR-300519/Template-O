7a13f2e7d56b70d92b5fd7fa9c941b8076a5d408
EAAR-300519 <aspire5@outlook.es>
2021-06-21 01:51:16 -0500

    [Update] correos sin confirmar

--- enviar.php
+++ enviar.php
@@ -8,11 +8,14 @@ $correo = $_POST['correo'];
 $numero = $_POST['numero'];
 $mensaje = $_POST['mensaje'];
 
+echo "Hola $nombre tu puesto es: $puesto tu empresa es $empresa las etiquetas que deseas son $area 
+la cantidad son $etiquetas su correo es $correo y su número es $numero";
+
 $header = 'From: ' . $correo . " \r\n";
-/* $header .= "X-Mailer: PHP/" . phpversion() . " \r\n"; */
-/* $header .= "Mime-Version: 1.0 \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
- */
+
 $message = "Este mensaje fue enviado por: " . $nombre . " \r\n";
 $message = "Empresa: " . $empresa . " \r\n";
 $message .= "Su e-mail es: " . $correo . " \r\n";
